e25c8e49528c25822a1f2f69ec2f38230466d4c2
Abigail Paulson <abigailpaulson@gatech.edu>
2019-07-28 13:44:41 -0400

    update variable instructions

--- kilosort2-pipeline/Kilosort2_Pipeline.m
+++ kilosort2-pipeline/Kilosort2_Pipeline.m
@@ -9,15 +9,18 @@
 clear; close all;
 
 %% Set parameters
-% Animal and day should be 1xN vect of same length. Files
-% should be cell array with each element containing vect of desired files.
-% probe channels should be cell array of same length as brainReg, with each
-% element containing channels of desired region. brainreg can be {''} if
-% desired. 
+% animal and day: 1xN vect of same length 
+% files: cell array with each element containing 1xN vect of desired files
+% probeChannels: cell array with each element containing 1xN vect of 
+%   channels of desired region. channels should be 0 based. 
+% brainReg: cell array of brain regions with same length as probeChannels. 
+%    can be {''} if desired
+% animalID: cell array of ID letters with same length as brainReg
 %
 % NOTE: multiple brainReg only debugged for INTAN, need to implement for
 % spike gadgets ALP 7/14/19
 
+%Test - Abby Intan
 animal = 7;             
 day = 190214;
 files = {1};  
@@ -29,19 +32,31 @@ clusterdir = 'C:\Users\apaulson3\Desktop\KilosortTesting\Spike Gadgets\';
 processeddatadir = 'Y:\singer\ProcessedData\Flicker_7Day_CA1CA3\';
 clusfolder = 'sorted\';
 
+%Test - Nuri Spike GAdgets
+% animal = 1;             
+% day = 190619;
+% files = {1};  
+% probeChannels = {1:32}; 
+% brainReg = {''}; 
+% animalID = {'N'};
+% rawdatadir = 'C:\Users\apaulson3\Desktop\KilosortTesting\Spike Gadgets\CA3\'; 
+% clusterdir = 'C:\Users\apaulson3\Desktop\KilosortTesting\Spike Gadgets\CA3\';
+% % processeddatadir = 'Y:\singer\ProcessedData\Flicker_7Day_CA1CA3\';
+% clusfolder = '';
+
 %% Set run options
 % writeToBin - first step, run to get .bin for Kilosort2
 % getSingleUnitTimes - run after manual curation in Phy2
 
-writeToBIN = 0; 
+writeToBIN = 1; 
 getSingleUnitTimes = 0; 
-getWFstruct = 1;
-applyQualityMetrics = 0; 
+getWFstruct = 0;
+qualityMetrics = 0; 
 
 %% set rewriting options
-% set these options to force the code to rewrite the filtered EEG files, or
-% the waveform files. Otherwise, the pipeline will load up previously
-% stored files if they exist. 
+% set these options to force the code to rewrite the files specified below. 
+% Otherwise, the pipeline will load up previously stored files if they 
+% exist. 
 
 rewrite.eeg = 0;
 rewrite.wf = 1;
@@ -89,6 +104,19 @@ if getWFstruct
 end
 
 %% apply quality metrics and make final clusters structure
+% Things I know I can do: SNR, ISI
+% Things I am not sure about: isolation against other units, noise overlap 
 
-if applyQualityMetrics 
+th.SNR =  1;                    % >= 1 SNR
+th.ISI = 0.0001;                % <= 0.01% refractory period violations
+th.refractoryPeriod = 0.001;    % 1ms refractory period duration
+th.noiseOverlap
+th.isolation
+
+if qualityMetrics 
+    for d = 1:length(day)
+        for br = 1:length(brainReg)
+            
+        end
+    end
 end
