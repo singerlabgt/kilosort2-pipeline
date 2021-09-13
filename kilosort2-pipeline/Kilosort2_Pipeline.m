9d34348c207a339bceb5c517211d69df3638a525
Steph Prince <stephanie.m.prince1@gmail.com>
2021-09-13 13:10:05 -0400

    update pipeline for running multiple sessions at once, transferring data

--- kilosort2-pipeline/Kilosort2_Pipeline.m
+++ kilosort2-pipeline/Kilosort2_Pipeline.m
@@ -13,22 +13,19 @@ clear; close all;
 %input animal info and project settings
 [params, dirs, th] = userProfiles_K2pipeline('Steph', 'UpdateTask');
 
-params.animal = 20;
-params.day = 210512;
+animals = [20];
+days = [210510:210521];
 
-%get the animal info based on
-allindexT = selectindextable(dirs.spreadsheetdir, 'animal', params.animal, 'datesincluded', params.day);
+%get the animal info based on the inputs
+allindexT = selectindextable(dirs.spreadsheetdir, 'animal', animals, 'datesincluded', days);
 allindex = allindexT{:,{'Animal', 'Date','Recording'}};
-params.files = {allindex(:,3)};
-
 [sessions, ind] = unique(allindex(:,1:2), 'rows'); %define session as one date
-params.brainReg = allindexT{ind,{'RegAB','RegCD'}};
 
 %% Set run options
 %First, run the preCuration step. 
 %After manually curation the Kilosort2 output, run the postCuration step. 
 
-run.preCuration = 0;            %write specificed files to .bin for Kilosort
+run.preCuration = 1;            %write specificed files to .bin for Kilosort
 run.kilosortScript = 1;         %run kilosort spike sorting using main_kilosort script
 run.kilosortGUI = 0;            %run kilosort spike sorting using the gui
 run.transferPrecuratedData = 1; %automatically transfer precurated data to server, removes locally
@@ -42,26 +39,35 @@ rewrite.eeg = 0;
 rewrite.wf = 1;
 rewrite.qualitymetrics = 1;
 
-%% write raw recording files to BIN for kilosort2
-
-if run.preCuration
-    for d = 1:length(params.day)
-        anrawdatadir = [dirs.rawdatadir, params.animalID, num2str(params.animal(d)), '_', num2str(params.day(d)), '\'];
-        tempfiledir = [dirs.processeddatadir, params.animalID, num2str(params.animal(d)), '_', num2str(params.day(d)), '\'];
-        anclusterdir = [dirs.localclusterdir, params.animalID, num2str(params.animal(d)), '_', num2str(params.day(d)), '\'];
+%% Run Kilosort Pipeline
+for d = 1:numel(sessions)
+    %% get the animal/day info
+    sessionInfo = allindex(ismember(allindex(:,1:2), sessions(d,:), 'rows'),:);
+    params.files = sessionInfo(:,3);
+    params.animal = sessionInfo(1,1);
+    params.day =  sessionInfo(1,2);
+    params.brainReg = allindexT{allindexT.Date == params.day & allindexT.Recording == params.files(1),{'RegAB','RegCD'}};
+    
+    %% write raw recording files to BIN for kilosort2
+    if run.preCuration
+        anrawdatadir = [dirs.rawdatadir, params.animalID, num2str(params.animal), '_', num2str(params.day), '\'];
+        tempfiledir = [dirs.processeddatadir, params.animalID, num2str(params.animal), '_', num2str(params.day), '\'];
+        anclusterdir = [dirs.localclusterdir, params.animalID, num2str(params.animal), '_', num2str(params.day), '\'];
         
         if ~exist(anclusterdir, 'dir'); mkdir(anclusterdir); end
-        converttoBIN_K2(anrawdatadir, anclusterdir, params.files{d}, params.probeChannels, params.brainReg, dirs.clusfolder)
+        converttoBIN_K2(anrawdatadir, anclusterdir, params.files, params.probeChannels, params.brainReg, dirs.clusfolder)
     end
-end
-
-
-%% run kilosort algorithm
-if run.kilosortScript
-    for d = 1:length(params.day)
+    
+    %% run kilosort algorithm
+    if run.kilosortGUI
+        kilosort
+        pause
+    end
+    
+    if run.kilosortScript
         for br = 1:length(params.brainReg)
             %run kilosort algorithm on data
-            anid = [params.animalID, num2str(params.animal(d)), '_', num2str(params.day(d))];
+            anid = [params.animalID, num2str(params.animal), '_', num2str(params.day)];
             anclusterdir = fullfile(dirs.localclusterdir, anid, params.brainReg{br}, dirs.clusfolder, 'kilosort', filesep);
             channels = max(params.probeChannels{br})-min(params.probeChannels{br})+1;
             main_kilosort(anclusterdir, dirs, params, channels)
@@ -73,34 +79,28 @@ if run.kilosortScript
             end
         end
     end
-elseif run.kilosortGUI
-    kilosort
-    pause
-end
+    
+    %% curate the data manually
+    % run the following lines in a GitBash or Anaconda terminal
+    % Note: the exact path is the output of 'anclusterdir' in matlab
+    % Note: if the files have been transferred to the server, you will likely
+    % want to transfer them back to the local computer before curating
 
+    % cd "C:\Users\YourGTID\Desktop\TempKilosort\YourSessionID\YourBrainRegion\sorted\kilosort\"
+    % conda activate phy2
+    % phy template-gui params.py
+    
+    %% get single unit times, waveforms, cluster properties, and apply quality metrics
 
-%% curate the data manually
-% run the following lines in a GitBash or Anaconda terminal
-% Note: the exact path of your directory is the output of 'anclusterdir' in 
-% matlab
-
-% cd "C:\Users\YourGTID\Desktop\TempKilosort\YourSessionID\YourBrainRegion\sorted\kilosort\s"
-% conda activate phy2
-% phy template-gui params.py
-
-
-%% get single unit times, waveforms, cluster properties, and apply quality metrics
-
-if run.postCuration
-    for d = 1:length(params.day)
+    if run.postCuration
         for br = 1:length(params.brainReg)
-            recinfo.iden = params.animalID; 
-            recinfo.index = [params.animal(d) params.day(d)]; 
-            recinfo.files = params.files{d}; 
-            recinfo.brainReg = params.brainReg{br}; 
+            recinfo.iden = params.animalID;
+            recinfo.index = [params.animal params.day];
+            recinfo.files = params.files;
+            recinfo.brainReg = params.brainReg{br};
             
-            anprocesseddatadir = [dirs.processeddatadir, params.animalID, num2str(params.animal(d)), '_', num2str(params.day(d)), '\', params.brainReg{br}, '\'];
-            anclusterdir = fullfile(dirs.localclusterdir, [params.animalID, num2str(params.animal(d)), '_', num2str(params.day(d))], params.brainReg{br}, dirs.clusfolder, '\');
+            anprocesseddatadir = [dirs.processeddatadir, params.animalID, num2str(params.animal), '_', num2str(params.day), '\', params.brainReg{br}, '\'];
+            anclusterdir = fullfile(dirs.localclusterdir, [params.animalID, num2str(params.animal), '_', num2str(params.day)], params.brainReg{br}, dirs.clusfolder, '\');
             figdir = fullfile(anclusterdir, 'figs');
             
             %get information about the curated units from the kilsort and
@@ -116,3 +116,8 @@ if run.postCuration
         end
     end
 end
+
+
+
+
+
